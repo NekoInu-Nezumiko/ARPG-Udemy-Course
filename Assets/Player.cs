64a6f72ad685d7b660b1894f57027189001c2959
Toraneko <nekoblack0101@gmail.com>
2023-09-12 08:51:17 +0900

    1-10

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour{
 
-    public float xInput;
+    public Rigidbody2D rb; //剛体(アタッチ)
+    public float moveSpeed;
+    public float jumpForce;
+    private float xInput;
 
     // Start is called before the first frame update
     void Start(){
@@ -14,5 +17,10 @@ public class Player : MonoBehaviour{
     // Update is called once per frame
     void Update(){
         xInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 }
