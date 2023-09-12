d9db580db73331afae7ac19bdae7dd169d256d91
Toraneko <nekoblack0101@gmail.com>
2023-09-12 16:31:38 +0900

    test

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -17,8 +17,9 @@ public class Player : MonoBehaviour{
     // Update is called once per frame
     void Update(){
         xInput = Input.GetAxisRaw("Horizontal");
+        //入力により速度変更
         rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
-
+        //Space　- Jump
         if(Input.GetKeyDown(KeyCode.Space)){
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
