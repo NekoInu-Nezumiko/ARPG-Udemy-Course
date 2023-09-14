c4d1dbf910150ea894549d148cf5a1746440863b
Toraneko <nekoblack0101@gmail.com>
2023-09-14 16:51:13 +0900

    1-15

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,24 +4,60 @@ using UnityEngine;
 
 public class Player : MonoBehaviour{
 
-    public Rigidbody2D rb; //剛体(アタッチ)
-    public float moveSpeed;
-    public float jumpForce;
-    private float xInput;
+    //Inspectorに表示する理由はないのでコンポーネントを取得する
+    private Rigidbody2D rb; //剛体(アタッチ)
+    private Animator anim; //Animator
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    private float xInput; //Arrow-Key
+    private int facingDir = 1; // right == 1 , left == -1
+    private bool facingRight = true;  //right == true , left == false
 
-    // Start is called before the first frame update
     void Start(){
-        
+        rb = GetComponent<Rigidbody2D>(); //ComponentにRigidbody2Dがあれば割り当てる
+        anim = GetComponentInChildren<Animator>(); //ChildにAnimatorがあれば割り当てる
     }
 
-    // Update is called once per frame
     void Update(){
+        Movement(); //Move
+        CheckInput(); //入力チェック
+        FlipController(); //反転チェック
+        AnimatorControllers();//Animation
+    }
+
+    private void CheckInput(){
+        //Arrow - Move
         xInput = Input.GetAxisRaw("Horizontal");
-        //入力により速度変更
-        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
         //Space　- Jump
-        if(Input.GetKeyDown(KeyCode.Space)){
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (Input.GetKeyDown(KeyCode.Space)){
+            Jump();
         }
+    }
+
+    private void Movement(){
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump(){
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void AnimatorControllers(){
+        //Animator - Move
+        bool isMoving = rb.velocity.x != 0; //速度が0でなければTrue
+        anim.SetBool("isMoving", isMoving);
+    }
+
+    private void Flip(){
+        facingDir *=  -1;
+        facingRight = !facingRight;
+        transform.Rotate(0,180,0);
+    }
+
+    private void FlipController(){
+        if (rb.velocity.x > 0 && !facingRight)
+            Flip();
+        else if(rb.velocity.x < 0 && facingRight)
+            Flip();
     }
 }
